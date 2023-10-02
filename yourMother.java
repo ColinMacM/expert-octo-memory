da10081ef0b1c8b802f0616be29d815ae22a61f1
Colin <65253497+ColinMacM@users.noreply.github.com>
2023-10-01 22:22:54 -0400

    Add files via upload

---
+++ yourMother.java
@@ -0,0 +1,11 @@
+class yourMother 
+{
+   int num;
+   int den;
+   public static void main(String args[])
+   {
+      yourMother f1 = new yourMother();
+      f1.num = 1;
+      f1.den = 5;
+   }
+}
